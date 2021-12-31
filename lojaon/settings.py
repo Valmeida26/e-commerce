f492467ed08ee1759a907d64e94121476b6ded91
erick <erick@gmail.com>
2021-12-31 00:06:30 -0300

    Add debug toolbar

--- lojaon/settings.py
+++ lojaon/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "pages.apps.PagesConfig",
     'products.apps.ProductsConfig',
    
+    'debug_toolbar',
 
 ]
 
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'lojaon.urls'
@@ -138,3 +140,10 @@ AUTH_USER_MODEL = "users.User"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
+# django-debug (se esta usando docker)
+
+import socket  # noqa
+
+hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+INTERNAL_IPS = [ip[:-1] + "1" for ip in ips]
